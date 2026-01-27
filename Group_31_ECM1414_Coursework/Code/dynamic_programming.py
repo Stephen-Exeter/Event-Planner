09be8d374312598d3c8e34de7cac1a9e98e66fc4
Stephen-Exeter <st1018@exeter.ac.uk>
2026-01-27 17:58:04 +0000

    vague content of the algorithm

--- Group_31_ECM1414_Coursework/Code/dynamic_programming.py
+++ Group_31_ECM1414_Coursework/Code/dynamic_programming.py
@@ -1,2 +1,11 @@
-def Dynamic_Programming():
-    ...
+
+
+def Dynamic_Programming(file_contents):
+    list_set_of_activities = []
+    table = [[],
+             []]
+    
+    activities = ...
+    total_enjoyment = ...
+    return {"selected_activities":activities,
+            "total enjoyment":total_enjoyment}
