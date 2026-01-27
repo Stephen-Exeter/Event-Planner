00214ddcd65bb6f510d6132f985997eb204f94fa
Stephen-Exeter <st1018@exeter.ac.uk>
2026-01-27 17:33:37 +0000

    importing funcitons and seting up how to time them

--- Group_31_ECM1414_Coursework/Code/event_planner.py
+++ Group_31_ECM1414_Coursework/Code/event_planner.py
@@ -7,12 +7,18 @@
 
 # CONSTRAINT BEING CODED FIRST : TIME
 
+from brute_force import Brute_Force
+from dynamic_programming import Dynamic_Programming
+import timeit
 
-def BruteForce(Constraint, InputData):
-    maxHappiness = 0
-    #loop through each combo, find max happiness
-    #if max happiness is higher than current highest, replace
-    pass
+# def BruteForce(Constraint, InputData):
+#     maxHappiness = 0
+#     #loop through each combo, find max happiness
+#     #if max happiness is higher than current highest, replace
+#     pass
 
-def DynamicAlgor():
-    pass
+# def DynamicAlgor():
+#     pass
+
+print(timeit.timeit("Brute_Force()"))
+print(timeit.timeit("Dynamic_Programming()"))
