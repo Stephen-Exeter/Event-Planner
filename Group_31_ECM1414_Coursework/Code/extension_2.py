0c99f811fd5dcf02a272e6cf00b96315e0c56207
illia-murza <im525@exeter.ac.uk>
2026-02-03 18:17:47 +0000

    Create few empty functions needed for extension 2

--- Group_31_ECM1414_Coursework/Code/extension_2.py
+++ Group_31_ECM1414_Coursework/Code/extension_2.py
@@ -1,5 +1,23 @@
 
+# A function to find execution time for brute force algorithm
+
+def brute_force_time():
+  pass
+
+
+# A function to find execution time for dynamic programming algorithm
+
+def dynamic_programming_time():
+  pass
+
+
 # A function which creates a line graph comparing the execution time of brute force vs. dynamic programming as n increases (time on y-axis, n on x-axis)
 
 def comparing_algorithms_line_graph():
+  pass
+
+
+# A function which creates a bar chart showing the "speedup factor" (brute force time ÷ dynamic programming time) for each input size.
+
+def speedup_factor_bar_chart():
   pass
