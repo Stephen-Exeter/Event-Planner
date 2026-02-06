c52599007ece1dc0652816b86407e5e4a9e14720
illia-murza <im525@exeter.ac.uk>
2026-02-06 12:21:06 +0000

    time functions

--- Group_31_ECM1414_Coursework/Code/extension_2.py
+++ Group_31_ECM1414_Coursework/Code/extension_2.py
@@ -1,14 +1,24 @@
+import timeit
+from take_input import *
+from brute_force import *
+from dynamic_programming import *
 
 # A function to find execution time for brute force algorithm
 
 def brute_force_time():
-  pass
+  start = timeit.timeit()
+  Brute_Force(read_file())
+  end = timeit.timeit()
+  return end - start
 
 
 # A function to find execution time for dynamic programming algorithm
 
 def dynamic_programming_time():
-  pass
+  start = timeit.timeit()
+  Dynamic_Programming(read_file())
+  end = timeit.timeit()
+  return end - start
 
 
 # A function which creates a line graph comparing the execution time of brute force vs. dynamic programming as n increases (time on y-axis, n on x-axis)
