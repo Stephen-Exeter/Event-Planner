d03aa74477b7ffad62346960525c3aa8b06174e4
Stephen-Exeter <st1018@exeter.ac.uk>
2026-02-08 12:36:15 +0000

    Dyanmic programming code which should work

--- Group_31_ECM1414_Coursework/Code/dynamic_programming.py
+++ Group_31_ECM1414_Coursework/Code/dynamic_programming.py
@@ -1,20 +1,19 @@
-def Dynamic_Programming(num_events,max_hours,max_budget,event_dict):
+def Dynamic_Programming(num_events:int ,max_hours:str ,max_budget:str ,event_dict:dict):
 
-    # first idea of how this should work
-    # consider as if you were doing all of the activities at first
-    # find a value such as enjoyment/time or budget to show which activity is the most efficient for enjoyment
-    # ignore the least efficient value and stop doing future considerations for that part of the table
-    # store all of the efficiency values so don't need to recalculate how efficient each activity is
-    # henceforth use the efficiency values and pick the most efficient each time? Doesn't seem like it would be what they want
-    # try using stacks or queues
-    # 
-    # just use Dijkstra's algorithm idk how with 2/3 varibales to consider but \_()_/ 
-
-    list_set_of_activities = []
-    table = [[],   # make the number of lists within the list the number of activities
-             []]
+    table = [[[],0] for _ in range(int(max_hours) + 1)] #all amounts of time possible
+                                                        #and the +1 to store the final result
     
-    activities = ...
-    total_enjoyment = ...
-    return {"selected_activities":activities,
-            "total enjoyment":total_enjoyment}
+    for name, list_vals in event_dict.items():
+        time,cost,enjoyment = list_vals
+        print(name)
+        for hours in range(int(max_hours),time-1,-1):
+            hours_left = hours - time
+            print(hours_left)
+            
+            if table[hours_left][1] + enjoyment > table[hours][1]:
+                table[hours][0] = table[hours_left][0].append(name)
+                table[hours][1] = table[hours_left][1] + enjoyment
+
+    print(table[int(max_hours)])
+
+    return table[int(max_hours)]
