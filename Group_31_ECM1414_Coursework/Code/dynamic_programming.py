3a09d321307709cc2fa23bedcfbb92f913153d50
Stephen-Exeter <st1018@exeter.ac.uk>
2026-02-09 11:52:42 +0000

    removing the prints and some extra comments

--- Group_31_ECM1414_Coursework/Code/dynamic_programming.py
+++ Group_31_ECM1414_Coursework/Code/dynamic_programming.py
@@ -1,19 +1,16 @@
-def Dynamic_Programming(num_events:int ,max_hours:str ,max_budget:str ,event_dict:dict):
+def Dynamic_Programming(num_events:int ,max_hours:str ,max_budget:str ,event_dict:dict) -> list:
 
     table = [[[],0] for _ in range(int(max_hours) + 1)] #all amounts of time possible
                                                         #and the +1 to store the final result
     
     for name, list_vals in event_dict.items():
         time,cost,enjoyment = list_vals
-        print(name)
+        
         for hours in range(int(max_hours),time-1,-1):
             hours_left = hours - time
-            print(hours_left)
             
             if table[hours_left][1] + enjoyment > table[hours][1]:
-                table[hours][0] = table[hours_left][0] + [name]
-                table[hours][1] = table[hours_left][1] + enjoyment
+                table[hours][0] = table[hours_left][0] + [name] # keep track of which events have been chosen on path
+                table[hours][1] = table[hours_left][1] + enjoyment # the total enjoyment of those chosen events
 
-    print(table[int(max_hours)])
-
-    return table[int(max_hours)]
+    return table[int(max_hours)]   # returned list will be [ [events selected] , total enjoyment ]
