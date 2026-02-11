81e0f919869bd4b19e1221973433deecf0cb29c0
Stephen-Exeter <st1018@exeter.ac.uk>
2026-02-11 13:15:25 +0000

    Adding code for how input will be taken when done testing

--- Group_31_ECM1414_Coursework/Code/event_planner.py
+++ Group_31_ECM1414_Coursework/Code/event_planner.py
@@ -15,8 +15,6 @@ def Final_Output(input_file):
     no_events, hours_max, budget_max, events = read_file(input_file) 
 
     # runs the brute force algorithm for finding the max happiness
-
-    no_events,hours_max,budget_max,events = read_file(input_file) 
     event_list_brute_force , enjoyment_brute_force , time_brute_force = Brute_Force(no_events,
                                                                                     hours_max,
                                                                                     budget_max,
@@ -30,7 +28,7 @@ def Final_Output(input_file):
     
     # print statemnent for all found values
 
-    print(f"""Event Planner results
+    print(f"""\n||Event Planner results||
           
             -Available time: {hours_max}
             -Available budget: {budget_max}
@@ -64,3 +62,16 @@ if __name__ == "__main__":
     Final_Output(r"Group_31_ECM1414_Coursework\Input_Files\input_small.txt")
     Final_Output(r"Group_31_ECM1414_Coursework\Input_Files\input_medium.txt")
     Final_Output(r"Group_31_ECM1414_Coursework\Input_Files\input_large.txt")
+
+    #this will be for when we are finished
+    # how_many_times = int(input("How many different input files do you have?"))
+    # for _ in range(how_many_times):
+    #     no_correct_file = True
+    #     while no_correct_file:
+    #         try:
+    #             file_path = input("\nEnter the file path to an input file: ")
+    #             Final_Output(file_path)
+    #             no_correct_file = False
+    #         except Exception as e:
+    #             print("File not found or not in correct format.")
+    #             print("The error: ", e)
