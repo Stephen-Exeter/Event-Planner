aa86eeb904ead8cb48f12806967409e26a4ba690
illia-murza <im525@exeter.ac.uk>
2026-02-13 14:24:37 +0000

    getting real data on a graph

--- Group_31_ECM1414_Coursework/Code/extension_2.py
+++ Group_31_ECM1414_Coursework/Code/extension_2.py
@@ -8,39 +8,90 @@ from take_input import *
 from brute_force import *
 from dynamic_programming import *
 
-# A function to find execution time for brute force algorithm
+"""
+def example():
+  link = "Group_31_ECM1414_Coursework/Input_Files"
+  list = link.listdir()
+  return list
+print(example())
+"""
 
-def brute_force_time():
-  start = timeit.timeit()
-  Brute_Force(read_file())
-  end = timeit.timeit()
-  no_of_events = read_file()[0]
-  return end - start, no_of_events
+def choose_input(): # getting data from all input files
+  list = ["Group_31_ECM1414_Coursework/Input_Files/input_small.txt",
+          "Group_31_ECM1414_Coursework/Input_Files/input_medium.txt",
+          "Group_31_ECM1414_Coursework/Input_Files/input_large.txt",
+          ]
+  input = []
+  for i in list:
+    input.append(read_file(i))
+  return input
 
 
-# A function to find execution time for dynamic programming algorithm
+def getting_data(): # replace tuples to lists in data from choose_input
+  list_0 = []
+  list_1 = []
+  for i in choose_input():
+    for k in i:
+      list_1.append(k)
+    list_0.append(list_1)
+    list_1 = []
+  return list_0
 
-def dynamic_programming_time():
-  start = timeit.timeit()
-  Dynamic_Programming(read_file())
-  end = timeit.timeit()
-  no_of_events = read_file()[0]
-  return end - start, no_of_events
+
+def brute_force_time(): # A function to find execution time for brute force algorithm
+  time = []
+  no_of_events = []
+
+  for i in getting_data():
+    no_of_events.append(i[0])
+
+    start = timeit.timeit()
+    Brute_Force(i[0], i[1], i[2], i[3])
+    end = timeit.timeit()
+    time.append(round(end - start, 6))
+
+  return time, no_of_events
+
+
+def dynamic_programming_time(): # A function to find execution time for dynamic programming algorithm
+  time = []
+  no_of_events = []
+
+  for i in getting_data():
+    no_of_events.append(i[0])
+
+    start = timeit.timeit()
+    Dynamic_Programming(i[0], i[1], i[2], i[3])
+    end = timeit.timeit()
+    time.append(round(end - start, 6))
+
+  return time, no_of_events
 
 
 # A function which creates a line graph comparing the execution time of brute force vs. dynamic programming as n increases (time on y-axis, n on x-axis)
-
-def comparing_algorithms_line_graph(brute_force_time,
-                                    dynamic_programming_time,
-                                    no_of_events):
+def comparing_algorithms_line_graph(brute_force_time=brute_force_time()[0],
+                                    dynamic_programming_time=dynamic_programming_time()[0],
+                                    no_of_events=brute_force_time()[1]):
   
-  plt.plot(no_of_events, brute_force_time)
-  plt.plot(no_of_events, dynamic_programming_time)
+  plt.plot(no_of_events, brute_force_time, label="brute force")
+  plt.plot(no_of_events, dynamic_programming_time, label="dynamic programming")
+
+  plt.title("Comparing time spent on algorithms")
+  plt.xlabel("Number of events")
+  plt.ylabel("Time spent")
 
   plt.savefig('Group_31_ECM1414_Coursework/graphs/line_graph.png')
-comparing_algorithms_line_graph([1, 14, 23], [2, 5, 7], [1, 2, 3])
+
+comparing_algorithms_line_graph()
+
 
 # A function which creates a bar chart showing the "speedup factor" (brute force time ÷ dynamic programming time) for each input size.
-
 def speedup_factor_bar_chart():
   pass
+
+
+
+
+
+print(f"brute: {brute_force_time()}")
+print(f"dynamic: {dynamic_programming_time()}")
